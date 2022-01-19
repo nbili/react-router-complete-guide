9ae2d42970a0db0b79f9c2dac70c01f2ab5a547c
nbili <coding520@163.com>
2022-01-20 00:27:44 +0800

    react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,52 @@
 import React from "react";
+import {
+  BrowserRouter as Router,
+  Link,
+  Navigate,
+  Route,
+  Routes,
+} from "react-router-dom";
+
+function ReactTopic() {
+  return <>ReactTopic</>;
+}
+
+function ReactDomTopic() {
+  return <>ReactDomTopic</>;
+}
+
+function ReactRouterDomTopic() {
+  return <>ReactRouterDomTopic</>;
+}
 
 function App() {
-  return <div>React Router</div>;
+  return (
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to={`/react`}>React</Link>
+          </li>
+          <li>
+            <Link to={`/react-dom`}>React Dom</Link>
+          </li>
+          <li>
+            <Link to={`/react-router-dom`}>React Router Dom</Link>
+          </li>
+        </ul>
+
+        <Routes>
+          <Route path={`/`} element={<Navigate to={`/react`} />}></Route>
+          <Route path={`/react`} element={<ReactTopic />}></Route>
+          <Route path={`/react-dom`} element={<ReactDomTopic />}></Route>
+          <Route
+            path={`/react-router-dom`}
+            element={<ReactRouterDomTopic />}
+          ></Route>
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
